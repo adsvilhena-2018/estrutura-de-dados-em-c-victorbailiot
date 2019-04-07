4b1bfeb2091688cdf31fb536c83341fefdb6f34f
victorbailiot <38817177+victorbailiot@users.noreply.github.com>
2019-04-06 22:35:57 -0400

    Update maxheap.c
    mudança de código

--- heap/arranjos/maxheap.c
+++ heap/arranjos/maxheap.c
@@ -7,28 +7,28 @@ int main()
     printf("Digite a quantidade de itens no Heap: ");
     scanf("%d", &no);
     printf("\nInsira os valores: ");
-    for (i = 0; i <  no; i++)
+    for (i = 0; i < no; i++)
         scanf("%d", &arr[i]);
-    for (i = 1; i <  no; i++)
+    for (i = 1; i < no; i++)
     {
         c = i;
         do
         {
-            heap  = (c - 1) / 2;
+            heap = (c - 1) / 2;
             if  (arr[heap] < arr[c])
             {
-                temp =  arr[heap];
+                temp = arr[heap];
                 arr[heap] = arr[c];
-                arr[c]  = temp;
+                arr[c] = temp;
             }
-            c =  heap;
+            c = heap;
         } while (c !=  0);
     }
 
     printf("Numeros digitados : ");
-    for (i = 0; i <  no; i++)
+    for (i = 0; i < no; i++)
         printf("%d\t ", arr[i]);
-    for (j = no - 1; j  >= 0; j--)
+    for (j = no - 1; j >= 0; j--)
     {
         temp = arr[0];
         arr[0] = arr[j];
@@ -36,20 +36,20 @@ int main()
         heap = 0;
         do
         {
-            c = 2 *  heap + 1;
+            c = 2 * heap + 1;
             if  ((arr[c] < arr[c + 1]) && c < j-1)
                 c++;
-            if  (arr[heap]<arr[c] && c<j)
+            if  (arr[heap] < arr[c] && c<j)
             {
-                temp =  arr[heap];
+                temp = arr[heap];
                 arr[heap] = arr[c];
-                arr[c]  = temp;
+                arr[c] = temp;
             }
             heap = c;
         } while (c  < j);
     }
-    printf("\nEm Max heap : ");
-    for (i = 0; i <  no; i++)
+    printf("\nEm Max heap: ");
+    for (i = 0; i < no; i++)
         printf("\t%d", arr[i]);
     printf("\n");
 }
