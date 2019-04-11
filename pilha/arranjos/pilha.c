cb4be11068e0681ad66bedda63e73bc5ac9429d7
victorbailiot <38817177+victorbailiot@users.noreply.github.com>
2019-04-11 00:03:44 -0400

    Update pilha.c
    add pilha estática

---
+++ pilha/arranjos/pilha.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int const MAXTAM = 1000;
+
+int Pilha[MAXTAM];
+int Topo;
+
+void Construtor(){
+    Topo = -1;
+}
+
+bool Pilha_Vazia(){
+    if(Topo==-1)
+        return true;
+    else
+        return false;
+
+    //return Topo==-1;
+}
+
+int Tamanho(){
+    return Topo+1;
+}
+
+bool Pilha_Cheia(){
+    if(Topo==MAXTAM-1)
+        return true;
+    else
+        return false;
+}
+
+bool Push(int valor){ //Push = Empilhar
+    if( Pilha_Cheia() ){
+       return false;
+    }else{
+        Topo++;
+        Pilha[Topo] = valor;
+        return true;
+    }
+}
+
+bool Pop(int &valor){ //Pop = Desempilhar
+
+    if( Pilha_Vazia() ){
+        return false;
+    }else{
+        valor=Pilha[Topo];
+        Topo--;
+        return true;
+    }
+}
+
+bool Pilha_Get(int &valor){ //Consulta
+    if( Pilha_Vazia() ){
+        return false;
+    }else{
+        valor=Pilha[Topo];
+        return true;
+    }
+}
+
+int main(){
+
+    int valor;
+
+    Construtor();
+    Push(5);
+    Push(7);
+    Push(9);
+    Push(1);
+    Push(2);
+
+    printf("\n\n Quant. itens da pilha:%d \n\n\n", Tamanho() );
+    
+    while( Pop(valor) ){
+        printf("\n %d \n",valor);
+    }
+    
+    printf("\n\n Quant. itens da pilha:%d \n\n\n", Tamanho() );
+
+    system("pause");
+    return 0;
+}
