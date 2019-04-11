cb7d07ee954b7d8d11fd5e6d0d181f78b20142f9
victorbailiot <38817177+victorbailiot@users.noreply.github.com>
2019-04-10 23:01:54 -0400

    Update fila.c
    Add Fila estática

---
+++ fila/arranjos/fila.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int const MAXTAM=1000;
+
+int Frente, Tras;
+int Fila[MAXTAM];
+
+void Fila_Construtor(){
+    Frente=0;
+    Tras=-1;
+}
+
+bool Fila_Vazia(){
+    if(Frente>Tras){
+        return true;
+    }else{
+        return false;
+    }
+
+    //return Frente>Tras;
+}
+
+bool Fila_Cheia(){
+    if(Tras==MAXTAM-1){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+bool Fila_Enfileirar(int valor){
+    if(Fila_Cheia()){
+        return false;
+    }else{
+        Tras++;
+        Fila[Tras]=valor;
+        return true;
+    }
+}
+
+bool Fila_Desenfileirar(int &valor){
+    if(Fila_Vazia()){
+        return false;
+    }else{
+        valor=Fila[Frente];
+        Frente++;
+        return true;
+    }
+}
+
+bool Fila_Get(int &valor){
+    if(Fila_Vazia()){
+        return false;
+    }else{
+        valor=Fila[Frente];
+        return true;
+    }
+}
+
+int Fila_Tamanho(){
+    return (Tras - Frente)+1;
+}
+
+
+int main(){
+    int Valor;
+    Fila_Construtor();
+
+    Fila_Enfileirar(2);
+    Fila_Enfileirar(5);
+    Fila_Enfileirar(4);
+
+    printf("\n\nTamanho:%d\n\n",Fila_Tamanho());
+
+
+    while( Fila_Desenfileirar(Valor) ){
+        printf("\nValor:%d\n",Valor);
+    }
+    printf("\n\nTamanho:%d\n\n",Fila_Tamanho());
+
+    system("pause");
+    return 0;
+}
